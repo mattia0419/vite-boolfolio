7ccb55092986532c990cebd4cce01d58e7080f23
mattia0419 <pironemattia6@gmail.com>
2023-11-07 19:52:17 +0100

    add pagination

--- src/components/posts/PostCard.vue
+++ src/components/posts/PostCard.vue
@@ -14,8 +14,18 @@ export default {
 <template>
     <div class="col-4">
         <div class="card h-100">
+            <div class="card-header d-flex justify-content-between align-items-start">
+                <span class="badge mx-1" :style="{ backgroundColor: post.type.color }">{{ post.type.name }}</span>
+                <div>
+                    <span class="badge rounded-pill mx-1" v-for="technology in post.technologies"
+                        :style="{ backgroundColor: technology.color }">{{ technology.label }}</span>
+                </div>
+            </div>
             <div class="card-body">
                 <h4>{{ post.title }}</h4>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-primary">Vedi</a>
             </div>
         </div>
     </div>
--- src/components/posts/PostList.vue
+++ src/components/posts/PostList.vue
@@ -6,13 +6,21 @@ export default {
     data() {
         return {
             posts: [],
-
+            pagination: {
+                next: null,
+                prev: null,
+                links: null
+            }
         }
     },
     methods: {
         fetchPosts(uri = store.api.baseUrl + 'posts') {
             axios.get(uri).then((response) => {
                 this.posts = response.data.data;
+
+                this.pagination.prev = response.data.prev_page_url;
+                this.pagination.next = response.data.next_page_url;
+                this.pagination.links = response.data.links;
             });
         }
     },
@@ -30,6 +38,18 @@ export default {
         <h2>PostList</h2>
         <div class="row g-3">
             <PostCard v-for="post in posts" :post="post"></PostCard>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item" v-for="link in pagination.links" @click="fetchPosts(link.url)"><a
+                            class="page-link" href="#" v-html="link.label"></a></li>
+                </ul>
+            </nav>
+            <!-- <div>
+                <a href="#" class="btn btn-primary" @click="fetchPosts(pagination.prev)">Prev</a>
+                <a href="#" v-for="link in pagination.links" class="btn btn-primary" @click="fetchPosts(link.url)">{{
+                    link.label }}</a>
+                <a href="#" class="btn btn-primary" @click="fetchPosts(pagination.next)">Next</a>
+            </div> -->
         </div>
     </div>
 </template>
